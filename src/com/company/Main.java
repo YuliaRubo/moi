ef73927054bec0f5171ff4dac2f567abc8574ba6
Ulya Ivancova <ulya-rubo@mail.ru>
2021-03-23 14:50:09 +0200

    comit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 7; i > 0; i--){
+            System.out.println(i);
+        }
+	// write your code here
+    }
+}
